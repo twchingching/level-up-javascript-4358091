52115a3b2da9e4e4c219ae8b91d73a7d166830b4
min <minchingkao1@gmail.com>
2023-12-16 13:33:03 -0600

    01_05_private_object.js

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,18 @@
 // Write your code here
+/*
+username
+password
+age
+*/
+
+const username = Symbol("username");
+const password = Symbol("password")
+const age = Symbol("8")
+
+const user = {
+  username: "David",
+  password: "test123",
+  age: "26"
+};
+
+console.log(user.username);
