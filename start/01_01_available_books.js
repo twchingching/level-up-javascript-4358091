83be649ed8076ce74285c70a8f7a16c468e62d6f
min <minchingkao1@gmail.com>
2023-12-16 10:31:18 -0600

    01_01_available_books.js
    creating a class to know the availability of the books amount etc.

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,40 @@
 // Write your code here
+class Book {
+
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability () {
+    this.getAvailability();
+  }
+
+  getAvailability () {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low in stock";
+    }
+    return "In stock";
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restorck(numCopiesStocked = 5){
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.availability);
+
+HungerGames.restock(12);
+console.log(HungerGames.availability);
+
+HungerGames.sell(17);
+console.log(HungerGames.availability);
