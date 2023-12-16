a94c536e3060cce5c65e19748c1fc31cfe1b777e
min <minchingkao1@gmail.com>
2023-12-16 11:15:04 -0600

    01_02_movie_prototype.js

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,21 @@
 // Write your code here
+class Movie {
+  constructor (movieTitle, movieDirector, movieGenre, movieReleaseYear, movieRating) {
+    this.movieTitle = movieTitle;
+    this.movieDirector = movieDirector;
+    this.movieGenre = movieGenre;
+    this.movieReleaseYear = movieReleaseYear;
+    this.movieRating = movieRating;
+  }
+
+  get overView() {
+    return this.getOverview();
+  }
+
+  getOverview() {
+    return `${this.movieTitle}, a ${this.movieGenre} file directed by ${this.movieDirector} was released in ${this.movieReleaseYear}. It received a rating of ${this.movieRating}`
+  }
+}
+
+const ToyStory = new Movie ("Toy Story", "David", "Action", "2022", "7.5")
+console.log(ToyStory.getOverview());
