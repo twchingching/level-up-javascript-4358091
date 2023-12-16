f649d3067dd55f337171bb53100118fbfe4630e8
min <minchingkao1@gmail.com>
2023-12-16 13:26:31 -0600

    fixing error in 01_04_technical_books.js

--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -31,9 +31,46 @@ class Book {
 }
 
 // Write your code here
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  // Getter
+  get availability() {
+    return this.getAvailability();
+  }
+
+  // Method
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In stock";
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+  
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+// Write your code here
+/*inhirent from Book
+ adding fifth adtion of Edition.
+ getEdition():string
+ */
 class TechnicalBook extends Book {
   constructor(title, author, ISBN, numCopies, edition) {
-    super(title, ISBN, author, numCopies);
+    super(title, author, ISBN, numCopies);
     this.edition = edition;
   }
 
@@ -41,7 +78,8 @@ class TechnicalBook extends Book {
     return `The current version of this book is ${this.edition}`;
   }
 
-  const CrackingTheCodingInterview = new TechnicalBook("Cracking The Coding Interview", "something")
-  console.log(CrackingTheCodingInterview.availability);
-  console.log(CrackingTheCodingInterview.getEdition());
 }
+
+  const CrackingTheCodingInterview = new TechnicalBook("Toy Story", "David", "12345", "5", "2nd");
+  console.log(CrackingTheCodingInterview.getEdition());
+
