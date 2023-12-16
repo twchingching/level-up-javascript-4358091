5d79e90a1de0828968defe59c90854d61432fb0e
min <minchingkao1@gmail.com>
2023-12-16 13:54:18 -0600

    01_07_classroom_attendance.js

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,25 @@
 // Write your code here
+/*
+ function getStudnets
+ boolean hasTechingAssistant
+
+ hasTechingAssistant = true 
+ => ["Teacher","Assistant", "student"]
+ hasTechingAssistant = false
+ => ["Teahcer","student"]
+*/
+
+function getStudents(classroom) {
+  let {hasTeacherAssistant, classList} = classroom;
+  let teacher, teachingAssistant, students;
+  if(hasTeacherAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+console.log(getStudents({
+  hasTeacherAssistant: false,
+  classList: ["David", "Michael", "John", "Bob"]
+}));
