f1e66001bbbeb6420ac7efa78992f920e9715956
min <minchingkao1@gmail.com>
2023-12-27 06:22:44 -0600

    01_12_string_permutation.js

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,9 @@
 // Write your code here
+function numberPermutations(letters) {
+  if (letters.length === 1) {
+    return 1;
+  }
+  return letters.length * numberPermutations(letters.slice(1));
+}
+
+console.log(numberPermutations("hello"));
